1b5366097edd87ae3273baf7da1c2a2a1d91b578
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2022-12-17 22:44:36 +0900

    add：フィボナッチ数列のアルゴリズムを組んでみる

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+app.listen(3000,console.log("サーバーが開始されました。"));
+
+// 計算処理半分にしたいので動的計画法
+function fib(n){
+    let a = 1;
+    let b = 1;
+    for(let i=3; i<=n; i++){
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+
+app.get("/", (req, res) => {
+    var result = fib(100);
+    console.log(result);
+    res.send("ウェブAPIをはやしていく");
+});
