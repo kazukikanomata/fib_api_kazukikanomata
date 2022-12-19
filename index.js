6c53b6ca9a47368a4343471a085d17272bacce75
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2022-12-19 17:53:49 +0900

    refac：コメントアウトを消してみた

--- index.js
+++ index.js
@@ -16,45 +16,25 @@ function fib(n){
     return b;
 }
 
-// 文字かどうかを判定
-// function isNaN(n){
-//     if (typeof n !== "string" || n === ""){
-//         // 文字列です
-//         return true;
-//     } else {
-//         // 文字列ではない
-//         return false;
-//     }
-// }
-
 // ステイータスコードが雑なのはだるいね。
 
-app.get("/", (req, res) =>{
-    return isString("ahdjkdhekk");
-});
-
 app.get("/fib", (req, res) => {
-    // req.query.n → numberのこと 具体例：n=10
     const input_value = req.query.n;
     const inputExists = isNaN(input_value);
     const hasPositiveNumber = Math.sign(input_value);
-    
-    // 計算した値。
     const result = fib(input_value);
-    if (inputExists == true){
+
+    if (inputExists === true){
         return res.status(400).json({
             "status": 400,
             "message": "数字を入力してください"
         });
     } else {
-        // プラスの時
         if (hasPositiveNumber === 1){
-            // 最終的な答え
             return res.status(200).json({
                 "status": 200,
                 "result": result
             });
-        //　0の時 
         }
         if (hasPositiveNumber === 0){
             return res.status(401).json({
