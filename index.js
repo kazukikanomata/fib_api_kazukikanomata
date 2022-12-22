a3edfb25c4a7a2959f2bc2df302ae739fe863f92
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2022-12-22 22:32:13 +0900

    change：デプロイ用にポートを変更

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const router = require('./routes/fib_root');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', router);
-app.listen(port);
+app.listen(PORT);
 
 console.log("サーバーが開始されました。");
